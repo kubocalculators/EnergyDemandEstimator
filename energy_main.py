c4b5fb4c68b307f823d6b5fb79203a0d055195f6
kubocalculators <kubo.teamcanada@gmail.com>
2026-03-16 14:21:27 -0400

    Update energy_main.py to version 3
    - Crop selection dropdown to auto-populate climate data
    - Crop data pdf download option
    - AHU selection built-in

--- energy_main.py
+++ energy_main.py
@@ -6,62 +6,107 @@ This script runs the web app user interface
 
 import streamlit as st
 import pandas as pd
-import helpers
+from helpers import prepare_weather_df, call_crop_temp_and_rh_setRanges, airflowrate_perAHU_m3h
 import active_cooling_v2
 import heating_v1
-import info_page
+import info_page_v2
 
 eta = 0.8   # This is the maximum allowable padwall efficiency
+crop_list = [
+    "TOMATO ON THE VINE, large",
+    "CHERRY TOMATO",
+    "BEEF TOMATO",
+    "CUCUMBER - LONG ENGLISH, high wire",
+    "CUCUMBER - LONG ENGLISH, traditional",
+    "CUCUMBER - SNACK/MINI, high wire",
+    "CUCUMBER - SNACK/MINI, traditional",
+    "STRAWBERRY",
+    "LETTUCE, baby leaf",
+    "LETTUCE, teen leaf (direct seeding)",
+    "LETTUCE, teen leaf (transplanted)",
+    "LETTUCE, whole head (medium)",
+    "LETTUCE, whole head (large)",
+    "SWEET POINT PEPPER",
+    "BELL PEPPER"
+]
 
 st.sidebar.title("Navigation")
 
 page = st.sidebar.radio("Go to:", ["Calculator","Info"], index=0)
 
+with st.sidebar:
+    with open("Productsheets.pdf", "rb") as f:
+        st.download_button(
+            "Open Crop Data PDF",
+            f,
+            file_name="Productsheets.pdf",
+            mime="application/pdf"
+        )
+
 if page == "Calculator":
 # ---------- STEP 1: Import climate data as Excel file ---------- #
     st.title("Energy Consumption Estimator")
-    st.header("Input Climate and Crop Information")
+    st.markdown(":red[**Red fields require attention.**]")
 
+    # Upload and clean climate data
+    st.header("Upload Climate Data")
+    st.markdown(
+        "**Make sure column titles match:**  \n"
+        "Local Time, Temperature (C), Relative Humidity (%), Solar Radiation (W/m²)")
+    weather_upload = st.file_uploader("Upload weather Excel from ksgclimatedata.streamlit.app", type=["xlsx"])
+
+    # Upload crop data
+    st.header("Upload Crop Data")
+    crop_name = st.selectbox(":red[**Select Crop**]", crop_list)
+    reference, variety, day_min_temp, day_max_temp, night_min_temp, night_max_temp, day_min_rh, day_max_rh, night_min_rh, night_max_rh, day_avg_temp, night_avg_temp, day_avg_rh, night_avg_rh = call_crop_temp_and_rh_setRanges(crop_name)
+
+    st.markdown("*Temperature and RH ranges will populate with crop selection.*")
+    # Display the default values, they can be overwritten
+    col1, col2 = st.columns(2)
+    with col1:
+        tmin_day = st.number_input("Day Min Temperature (C)", value=day_min_temp)
+        tmin_night = st.number_input("Night Min Temperature (C)", value=night_min_temp)
+        rh_cap_day = st.number_input("Day MAX RH (%)", value=day_max_rh)
+    with col2:
+        tmax_day = st.number_input("Day Max Temperature (C)", value=day_max_temp)
+        tmax_night = st.number_input("Night Max Temperature (C)", value=night_max_temp)
+        rh_cap_night = st.number_input("Night MAX RH (%)", value=night_max_rh)
+    
+    
+    st.header("Input Project Specifics")
     with st.form("inputData", clear_on_submit=False):
         
-        # Upload and clean climate data
-        st.subheader("Upload Climate Data")
-        st.markdown(
-            "**Make sure column titles match:**  \n"
-            "Local Time, Temperature (C), Relative Humidity (%), Solar Radiation (W/m²)")
-        weather_upload = st.file_uploader("Upload weather Excel from ksgclimatedata.streamlit.app", type=["xlsx"])
-        
+        st.subheader("Climate Setpoints")
         # Take in crop parameters
-        st.subheader("Crop Climate Parameters")
-        t_day = st.number_input("Day Temperature Setpoint (C)", step=1)
-        t_night = st.number_input("Night Temperature Setpoint (C)", step=1)
-        rh_day = st.number_input("Day RH Setpoint (%)", 0, 100, step=1)
-        rh_night = st.number_input("Night RH Setpoint (%)", 0, 100, step=1)
-        rh_cap_day = st.number_input("Day Max RH (%)", 0, 100, step=1)
-        rh_cap_night = st.number_input("Night Max RH (%)", 0, 100, step=1)
-        tmax_day = st.number_input("Day Max Temperature (C)", step=1)
-        tmax_night = st.number_input("Night Max Temperature (C)", step=1)
-        tmin_day = st.number_input("Day Min Temperature (C)", step=1)
-        tmin_night = st.number_input("Night Min Temperature (C)", step=1)
+        st.markdown("*Default values are average between min/max ranges.*")
+        t_day = st.number_input("Day Temperature Setpoint (C)", value=day_avg_temp)
+        t_night = st.number_input("Night Temperature Setpoint (C)", value=night_avg_temp)
+        rh_day = st.number_input("Day RH Setpoint (%)", 0.0, 100.0, value=day_avg_rh)
+        rh_night = st.number_input("Night RH Setpoint (%)", 0.0, 100.0, value=night_avg_rh)
 
         st.subheader("Greenhouse Parameters")
-        st.markdown("The airflow rate is per AHU.")
-        airflow_m3_h = st.number_input("Air Flow Rate (m3/h)", None, None, 18000) # default = 18,000 m3/h
-        st. markdown("The total number of AHUs is equal to the number of AHUs per truss x the number of trusses")
-        AHU_count = st.number_input("Total Number of AHUs", step=1)
-        st.markdown("The area per AHU is the truss width divided by the number of AHUs per truss and multiplied by the length of the air hose")
-        area_m2 = st.number_input("Area (m2) per AHU", None, None, 200) # default = 200 m2 per AHU
+        col11, col12 = st.columns(2)
+        with col11:
+            truss_count = st.number_input(":red[**Number of trusses**]", step=1)           
+            AHU_count_pertruss = st.number_input(":red[**Number of AHUs per truss**]", step=1)
+            trolley_selection = st.selectbox("Trolley Size", ["No trolley", "Standard (771mm)"])
+        with col12:
+            truss_length = st.number_input(":red[**Truss Length (mm)**]", value=9600, step=1)
+            airtube_length = st.number_input(":red[**Air Tube Length (m)**]", value=110)
+            st.markdown("*NOTE:  \n" \
+            "Ventilation Rate (m3/h) per AHU is calculated using this configuration to select a fan.*")
+
         cladd = st.number_input("Cladd Ratio", None, None, 1.2)
         u_leak = st.number_input("U_leak (W/m2/K)", None, None, 0.7)
         u_roof = st.number_input("U_roof (W/m2/K)", None, None, 6.9)
-        scr1_eff = st.number_input("Screen 1 Energy Efficiency (%)", 0, 100, 47, step=1)
-        scr2_eff = st.number_input("Screen 2 Energy Efficiency (%)", 0, 100, 50, step=1)
+        scr1_eff = st.number_input(":red[**Screen 1 Energy Efficiency (%)**]", 0, 100, 47, step=1)
+        scr2_eff = st.number_input(":red[**Screen 2 Energy Efficiency (%)**]", 0, 100, 50, step=1)
 
         st.subheader("Select Calculation Methods")
-        cooling_method = st.selectbox("Active Cooling Load Method", ["True Temperature and RH Setpoints","Maximum Allowed Temperature and Unlimited RH"])
-        heating_method = st.selectbox("Heating Load Method", ["True Setpoint","Minimum Allowed Temperature"])
-        hours_of_heat_storage = st.number_input("Hours of Heat Storage", step=1)
-        peak_percentile = st.selectbox("Peak Demand Percentile", [98, 95, 92.5, 90, 85])
+        cooling_method = st.selectbox("Active Cooling Load Method", ["Maximum Allowed Temperature and Unlimited RH", "True Temperature and RH Setpoints"])
+        heating_method = st.selectbox("Heating Load Method", ["Minimum Allowed Temperature", "True Setpoint"])
+        hours_of_heat_storage = st.number_input("Hours of Heat Storage", value=8, step=1)
+        peak_percentile = st.selectbox("Peak Demand Percentile", [95, 92.5, 90, 85])
 
         run = st.form_submit_button("Calculate")
 
@@ -71,7 +116,7 @@ if page == "Calculator":
     if run:
 
     # Clean data and append crop parameters to the weather DataFrame
-        weather_df = helpers.prepare_weather_df(
+        weather_df = prepare_weather_df(
             weather_upload,
             t_day,
             t_night,
@@ -86,6 +131,11 @@ if page == "Calculator":
         )
         st.success("Weather data successfully processed")
 
+    # Count AHU and AHU_area
+        AHU_count = AHU_count_pertruss * truss_count
+        area_m2 = truss_length / 1000 / AHU_count_pertruss * airtube_length
+        AHU_type, airflow_m3_h = airflowrate_perAHU_m3h(truss_length, trolley_selection, AHU_count_pertruss)
+    
     # Run heating load calculation
 
         if heating_method == "True Setpoint":
@@ -127,7 +177,9 @@ if page == "Calculator":
 
         st.subheader("Cooling Load Percentile Summary")
         st.dataframe(cooling_results)
+        st.markdown(f"AHU type **{AHU_type}** was selected and has a max ventilation rate of **{airflow_m3_h} m3/h**")
 
 elif page == "Info":
-    info_page.render()
+    info_page_v2.render()
+
 
